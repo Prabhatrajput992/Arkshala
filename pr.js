e31da6f072693dc7b313b7145d1bfe4d061b3f4d
Prabhatrajput992 <rajputprabhat4203@gmail.com>
2025-02-13 17:20:57 +0530

    slider or search bar

--- pr.js
+++ pr.js
@@ -14,3 +14,26 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+// slider
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.querySelectorAll(".slide");
+    
+    slides.forEach(slide => {
+        slide.style.display = "none";
+    });
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+showSlides();
